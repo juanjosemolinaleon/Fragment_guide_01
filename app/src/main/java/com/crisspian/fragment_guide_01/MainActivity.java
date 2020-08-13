4500e6bd7945317ff366c833b0ca959827ee11c2
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-12 23:09:33 -0400

    M3_13_Ej_02

--- app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -1,15 +1,36 @@
 package com.crisspian.fragment_guide_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
+
+import com.crisspian.fragment_guide_01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFragment();
+            }
+        });
+    }
+    private void showFragment(){
+        FirstFragment firstFragment = FirstFragment.newInstance("","");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content_fragment,firstFragment).addToBackStack(null).commit();
     }
 
 }
